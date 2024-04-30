b939dc938374d2fca32f7b71e357abcc0702ce0d
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-04-29 21:25:18 -0400

    Added few more tests, added array lenght validation after the mentor's review

--- tail.js
+++ tail.js
@@ -10,9 +10,12 @@ const assertEqual = function(actual, expected) {
 
 // FUNCTION IMPLEMENTATION
 const tail = function(array) {
+  if (array.length >= 1){
   return array.slice(1);
+} else {
+  return array;
+}
 };
-
 // Test Case: Check 3 values array
 const threeWords = tail(["Yo Yo", "Lighthouse", "Labs"]);
 assertEqual(threeWords.length, 2); // original array should still have 3 elements!
@@ -24,3 +27,13 @@ assertEqual(oneWord.length, 0); // original array should still have 3 elements!
 // Test Case: Empty array
 const zeroWords = tail([]);
 assertEqual(zeroWords.length, 0); // original array should still have 3 elements!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
