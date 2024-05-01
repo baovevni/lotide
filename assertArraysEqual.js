1b54b47968ef814cc958422608b0f821354b636a
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-04-30 20:51:06 -0400

    Implemented assertArraysEqual function based on eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays (arr1, arr2)){
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !=== ${arr2}`);
+  }
+}
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
