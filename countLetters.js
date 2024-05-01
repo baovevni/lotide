d7cc21027510f0858e2d300de0b85d8143383c47
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-01 18:39:32 -0400

    implemented countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+// assertArraysEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (letters of sentence) {
+    if (result[letters]) {
+      result[letters] += 1;
+    } else {
+      result[letters] = 1;
+    }
+  }
+  return result;
+};
+console.log(countLetters("lighthouse in the house"));
+
+
+// Test code
+const countLettersTest = countLetters("lighthouse in the house");
+assertEqual(countLettersTest.l, 1);
+assertEqual(countLettersTest.i, 2);
+assertEqual(countLettersTest.g, 1);
+assertEqual(countLettersTest.h, 4);
+assertEqual(countLettersTest.t, 2);
+assertEqual(countLettersTest.o, 2);
+assertEqual(countLettersTest.u, 2);
+assertEqual(countLettersTest.s, 2);
+assertEqual(countLettersTest.e, 3);
+assertEqual(countLettersTest.n, 1);
