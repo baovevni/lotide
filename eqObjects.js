ed874da06ce2cc19f93666a916ad75eaacd4052f
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-01 21:35:47 -0400

    last fixes implemented

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // assertEqual function
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -22,16 +22,18 @@ const assertEqual = function(actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
+const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let key of Object.keys(object1)) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])){
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else if (typeof object1[key] !== typeof object2[key]) {
+      return false;
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
