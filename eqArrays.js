99a747c639ea0e9b8b6520d57240991dce3d9679
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-04 00:34:17 -0400

    changed for in to c-style for loop as suggested by AI feedback

--- eqArrays.js
+++ eqArrays.js
@@ -12,17 +12,30 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let key in arr1) {
-    if (Array.isArray(arr1[key]) && Array.isArray(arr2[key])) {
-      if (!eqArrays(arr1[key], arr2[key])) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
-    } else if (arr1[key] !== arr2[key]) {
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
+
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   } else {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] !== arr2[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
