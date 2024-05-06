1388682cd0d728d195b8c1483aebd029dcebd8ca
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-06 10:54:41 -0400

    added a comment in eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -4,6 +4,7 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
+    // checking if both arr[1] and arr[2] are arrays and recursively calling eqArrays on these nested arrays
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
