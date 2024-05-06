07b0acde6c8cdeaa95ecbf79651fbc4fc5126a55
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-06 15:15:43 -0400

    added a test to cover empty array scenario

--- test/headTest.js
+++ test/headTest.js
@@ -10,5 +10,8 @@ describe("#head", () => {
   });
   it("returns 'Hello' for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-  })
+  });
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
 });
