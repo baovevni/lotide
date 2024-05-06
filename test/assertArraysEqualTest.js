5258938e6512e6a187b7632127f148d05dd7be21
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-06 11:11:50 -0400

    assertArraysEqual test removed eqArrys as requested by AI review

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,4 @@
 const assertArraysEqual = require("../assertArraysEqual");
-const eqArrays = require("../eqArrays");
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
